87efb8968181d340b3c1b2e8072bda2c2f426438
Aaron Russell <aaron@russell-tech.co.uk>
2023-03-05 12:40:05 +0000

    Changed person id for homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,7 +48,7 @@ export const pageQuery = graphql`
       }
     }
     allContentfulPerson(
-      filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }
+      filter: { contentful_id: { eq: "4Ff1pPPUTdU7JI822TLc7O" } }
     ) {
       nodes {
         name
