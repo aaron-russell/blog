9e12e33e34a15927ae81112fc4b5883f12509fb0
Aaron Russell <aaron@russell-tech.co.uk>
2023-03-18 22:42:35 +0000

    Added spam protection (temp)

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,8 +1,10 @@
 import staticFormsPlugin from '@cloudflare/pages-plugin-static-forms'
 import { v4 as uuidv4 } from 'uuid'
+import { AkismetClient } from 'akismet-api'
 
 interface Env {
   NAMESPACE: KVNamespace
+  AKISMET_KEY: string
 }
 
 export const onRequest: PagesFunction<Env> = (context) =>
@@ -12,14 +14,24 @@ export const onRequest: PagesFunction<Env> = (context) =>
       const message = formData.get('message')
       const nameOnForm = formData.get('name')
 
-      await context.env.NAMESPACE.put(
-        uuidv4(),
-        JSON.stringify({
-          name: nameOnForm,
-          email,
-          message,
-        })
-      )
+      const key = context.env.AKISMET_KEY
+      const blog = 'https://myblog.com'
+      const client = new AkismetClient({ key, blog })
+
+      const comment: {
+        name: string
+        email: string
+        message: string
+        isSpam?: boolean
+      } = {
+        name: nameOnForm,
+        email,
+        message,
+      }
+
+      const isSpam = await client.checkSpam(comment)
+      comment.isSpam = isSpam
+      await context.env.NAMESPACE.put(uuidv4(), JSON.stringify(comment))
 
       return Response.redirect(
         'https://aaron-russell.co.uk/contact?submitted=true'
