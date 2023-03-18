c0875db2b47a27f02a09b067365147cb8e49b4c9
Aaron Russell <aaron@russell-tech.co.uk>
2023-03-18 22:52:38 +0000

    Added try catch statements

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -10,31 +10,35 @@ interface Env {
 export const onRequest: PagesFunction<Env> = (context) =>
   staticFormsPlugin({
     respondWith: async ({ formData, name }) => {
-      const email = formData.get('email')
-      const message = formData.get('message')
-      const nameOnForm = formData.get('name')
+      try {
+        const email = formData.get('email')
+        const message = formData.get('message')
+        const nameOnForm = formData.get('name')
 
-      const key = context.env.AKISMET_KEY
-      const blog = 'https://myblog.com'
-      const client = new AkismetClient({ key, blog })
+        const key = context.env.AKISMET_KEY
+        const blog = 'https://aaron-russell.co.uk'
+        const client = new AkismetClient({ key, blog })
 
-      const comment: {
-        name: string
-        email: string
-        message: string
-        isSpam?: boolean
-      } = {
-        name: nameOnForm,
-        email,
-        message,
+        const comment: {
+          name: string
+          email: string
+          message: string
+          isSpam?: boolean
+        } = {
+          name: nameOnForm,
+          email,
+          message,
+        }
+
+        const isSpam = await client.checkSpam(comment)
+        comment.isSpam = isSpam
+        await context.env.NAMESPACE.put(uuidv4(), JSON.stringify(comment))
+
+        return Response.redirect(
+          'https://aaron-russell.co.uk/contact?submitted=true'
+        )
+      } catch (error) {
+        console.log(error)
       }
-
-      const isSpam = await client.checkSpam(comment)
-      comment.isSpam = isSpam
-      await context.env.NAMESPACE.put(uuidv4(), JSON.stringify(comment))
-
-      return Response.redirect(
-        'https://aaron-russell.co.uk/contact?submitted=true'
-      )
     },
   })(context)
