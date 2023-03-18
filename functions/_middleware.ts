9686e1fcee74908201022a2f3665879803490f8e
Aaron Russell <aaron@russell-tech.co.uk>
2023-03-18 23:03:31 +0000

    Added ip to comment

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,6 +1,6 @@
 import staticFormsPlugin from '@cloudflare/pages-plugin-static-forms'
 import { v4 as uuidv4 } from 'uuid'
-import { AkismetClient } from 'akismet-api'
+import { AkismetClient, Comment } from 'akismet-api'
 
 interface Env {
   NAMESPACE: KVNamespace
@@ -12,27 +12,26 @@ export const onRequest: PagesFunction<Env> = (context) =>
     respondWith: async ({ formData, name }) => {
       try {
         const email = formData.get('email')
-        const message = formData.get('message')
+        const content = formData.get('message')
         const nameOnForm = formData.get('name')
 
         const key = context.env.AKISMET_KEY
         const blog = 'https://aaron-russell.co.uk'
         const client = new AkismetClient({ key, blog })
 
-        const comment: {
-          name: string
-          email: string
-          message: string
-          isSpam?: boolean
-        } = {
+        const comment: Comment = {
+          user_ip: context.request.headers.get('CF-Connecting-IP') || '',
+          user_agent: context.request.headers.get('User-Agent') || '',
           name: nameOnForm,
           email,
-          message,
+          content,
         }
 
         const isSpam = await client.checkSpam(comment)
-        comment.isSpam = isSpam
-        await context.env.NAMESPACE.put(uuidv4(), JSON.stringify(comment))
+        await context.env.NAMESPACE.put(
+          uuidv4(),
+          JSON.stringify({ ...comment, isSpam })
+        )
 
         return Response.redirect(
           'https://aaron-russell.co.uk/contact?submitted=true'
