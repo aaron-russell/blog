b909441a961ae601c855fe0959edee504fea697f
Aaron Russell <aaron@russell-tech.co.uk>
2023-03-19 00:56:45 +0000

    Removed Akismet

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,11 +1,10 @@
 import staticFormsPlugin from '@cloudflare/pages-plugin-static-forms'
 import { v4 as uuidv4 } from 'uuid'
-import * as akismet from 'akismet'
 import { Toucan } from 'toucan-js'
 
 interface Env {
   NAMESPACE: KVNamespace
-  AKISMET_KEY: string
+  //   AKISMET_KEY: string
   SENTRY_DSN: string
 }
 
@@ -22,32 +21,19 @@ export const onRequest: PagesFunction<Env> = (context) =>
         const content = formData.get('message')
         const nameOnForm = formData.get('name')
 
-        const apiKey = context.env.AKISMET_KEY
-        const blog = 'https://aaron-russell.co.uk'
-        const client = akismet.client({
-          apiKey,
-          blog,
-          userAgent: `Node.js/16 | Akismet-api/6.0.0`,
-        })
-
         const comment: any = {
           user_ip: context.request.headers.get('CF-Connecting-IP') || '',
           user_agent: context.request.headers.get('User-Agent') || '',
           name: nameOnForm,
           email,
           content,
+          date: new Date().toISOString(),
         }
 
-        client.checkComment(comment, async (error: string, isSpam: boolean) => {
-          if (error) {
-            sentry.captureException(error)
-          } else {
-            await context.env.NAMESPACE.put(
-              uuidv4(),
-              JSON.stringify({ ...comment, isSpam })
-            )
-          }
-        })
+        await context.env.NAMESPACE.put(
+          uuidv4(),
+          JSON.stringify({ ...comment })
+        )
 
         return Response.redirect(
           'https://aaron-russell.co.uk/contact?submitted=true'
