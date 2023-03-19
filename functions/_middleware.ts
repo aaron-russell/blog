6fff1f65046f53e1fd5fb5f85076ea029fd4f592
Aaron Russell <aaron@russell-tech.co.uk>
2023-03-19 00:17:50 +0000

    Updated to Toucan

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,7 +1,7 @@
 import staticFormsPlugin from '@cloudflare/pages-plugin-static-forms'
 import { v4 as uuidv4 } from 'uuid'
 import { AkismetClient, Comment } from 'akismet-api'
-import * as Sentry from '@sentry/node'
+import { Toucan } from 'toucan-js'
 
 interface Env {
   NAMESPACE: KVNamespace
@@ -12,13 +12,10 @@ interface Env {
 export const onRequest: PagesFunction<Env> = (context) =>
   staticFormsPlugin({
     respondWith: async ({ formData, name }) => {
-      Sentry.init({
+      const sentry = new Toucan({
         dsn: context.env.SENTRY_DSN,
-        tracesSampleRate: 1.0,
-      })
-      const transaction = Sentry.startTransaction({
-        op: 'contact-form',
-        name: 'Contact Form',
+        context,
+        request: context.request,
       })
       try {
         const email = formData.get('email')
@@ -47,9 +44,7 @@ export const onRequest: PagesFunction<Env> = (context) =>
           'https://aaron-russell.co.uk/contact?submitted=true'
         )
       } catch (error) {
-        Sentry.captureException(error)
-      } finally {
-        transaction.finish()
+        sentry.captureException(error)
       }
     },
   })(context)
